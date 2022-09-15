a317955f177302d05d9fe60f3b2592204b28aba2
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-09-15 21:47:57 +0600

    Update pb_2_Amplitude_demodulated_signal.m

--- pb_2_Amplitude_demodulated_signal.m
+++ pb_2_Amplitude_demodulated_signal.m
@@ -1,48 +1,46 @@
-clc;
-clear all;
-close all;
-Vm = 10;
-Vc = 10;
-Fc = 30;
-Fm = 0.3;
-Fs = 130;
-t1 = 10;
-t = 0:1/Fs:t1;
-c = Vc*sin(2*pi*Fc*t);
-m = Vm*sin(2*pi*Fm*t);
-y = ammod(m,Fc,Fs);
-subplot(5,1,1);
-plot(t,m);
-title('Modulating Signal');
-xlabel('Time(s)');
-ylabel('Amplitude(V)');
-subplot(5,1,2);
-plot(t,c);
-title('Carrier Signal');
-xlabel('Time(s)');
-ylabel('Amplitude(V)');
-subplot(5,1,3);
-plot(t,y);
-title('Modulated Signal');
-xlabel('Time(s)');
-ylabel('Amplitude(V)');
-Vd(1)=0;
-for i=2:length(y)
- if y(i)>Vd(i-1)
- Vd(i)=y(i);
- else
- Vd(i)=Vd(i-1)-0.023*Vd(i-1);
- end
-end
-h=fir1(100,0.0125,'low');
-foutputc=filter(h,1,Vd);
-subplot(5,1,4);
-plot(t,Vd);
-title('Envelope detector output of modulating signal');
-xlabel('Time(s)');
-ylabel('Amplitude(V)');
-subplot(5,1,5);
-plot(t,foutputc);
-title('Demodulated signal');
-xlabel('Time(s)');
-ylabel('Amplitude(V)');
+fs = 1000;
+t = 0 : 1/fs : 1;
+fc = 100;
+fm = 10;
+am = 7;
+ac = 10;
+
+%% generating message signal 
+mt = am*sin(2*pi*fm*t);
+subplot(4,1,1);
+plot(t,mt);
+title('message signal ');
+xlabel('time');
+ylabel('amplitude');
+grid on;
+
+%% generating carrier  signal
+ct = ac*sin(2*pi*fc*t);
+subplot(4,1,2);
+plot(t,ct);
+title('message signal ');
+xlabel('time');
+ylabel('amplitude');
+grid on;
+
+%% generating modulated signal 
+k = 0.5;
+yt = ac.*(1+k*sin(2*pi*fm*t)).*sin(2*pi*fc*t);
+subplot(4,1,3);
+plot(t,yt);
+title('modulated signal ');
+xlabel('time');
+ylabel('amplitude');
+grid on;
+
+%% generating demodulated signal 
+r = abs(yt); %Rectified Signal
+[b,a] = butter(10,2*fc/fs);
+%[b,a] = butter(n,Wn) returns the transfer function coefficients of an nth-order lowpass digital Butterworth filter with normalized cutoff frequency Wn.
+mr = filter(b,a,r);
+subplot(4,1,4);
+plot(t,mr,'r');
+title('demodulated signal ');
+xlabel('time');
+ylabel('amplitude');
+grid on;
