32509d81c5ec9f086aa3fc72abf72b37fa4c0669
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-09-16 18:27:01 +0600

    Update pb_4_FM_Demodulation.m

--- pb_4_FM_Demodulation.m
+++ pb_4_FM_Demodulation.m
@@ -21,7 +21,7 @@ plot(t,ct);
 title("Carrier Signal");
 
 % modulated signal
-st=Ac*cos((wc*t)+B.*sin(wm*t));
+st=Ac*cos((wc*t)+B*sin(wm*t));
 subplot(5,1,3);
 plot(t,st);
 title("Modulated Signal");
